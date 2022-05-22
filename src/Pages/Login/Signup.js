ee57718bc437de69dec4684cf66fb09f2541cf58
RittikaDev <rittika.dev@gmail.com>
2022-05-22 21:13:10 +0600

    sign up with react hook form

--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import signupp from "../../images/signup.jpg";
 
 const Signup = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex mt-7 justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl mr-1.5 mt-4 ">
@@ -16,7 +26,32 @@ const Signup = () => {
       <div className="card w-96 bg-base-100 shadow-xl px-3">
         <div className="card-body">
           <h2 className="text-center text-2xl font-bold">SIGN UP</h2>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Your name"
+                className="input input-bordered w-full max-w-xs"
+                {...register("name", {
+                  required: { value: true, message: "Name is required" },
+                })}
+              />
+              <label className="label">
+                {errors.name?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.name.message}
+                  </span>
+                )}
+                {errors.name?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.name.message}
+                  </span>
+                )}
+              </label>
+            </div>
             <div className="form-control w-full max-w-xs">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -25,7 +60,26 @@ const Signup = () => {
                 type="email"
                 placeholder="Your Email"
                 className="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: { value: true, message: "Email is required" },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: "Provide a valid email", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
               />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
             </div>
             <div className="form-control w-full max-w-xs">
               <label className="label">
@@ -35,7 +89,26 @@ const Signup = () => {
                 type="password"
                 placeholder="Your Password"
                 className="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: { value: true, message: "Password is required" },
+                  minLength: {
+                    value: 6,
+                    message: "Must be six characters or longer", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
               />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
             </div>
             <div className="form-control w-full max-w-xs">
               <label className="label">
@@ -45,7 +118,26 @@ const Signup = () => {
                 type="password"
                 placeholder="Confirm Password"
                 className="input input-bordered w-full max-w-xs"
+                {...register("cpassword", {
+                  required: { value: true, message: "Password is required" },
+                  minLength: {
+                    value: 6,
+                    message: "Password & Confirm Password does not match", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
               />
+              <label className="label">
+                {errors.cpassword?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.cpassword.message}
+                  </span>
+                )}
+                {errors.cpassword !== errors.password && (
+                  <span className="label-text-alt text-red-500">
+                    Password & Confirm Password does not match
+                  </span>
+                )}
+              </label>
             </div>
             <input
               className="btn w-full max-w-xs mt-4"
